c9f560b17efc25d84bf6f70b53e809eb2c1f3be7
Matt <lumpychum@gmail.com>
2023-02-12 18:59:14 -0600

    implemented mergesort

--- src/collections/Sort.lua
+++ src/collections/Sort.lua
@@ -1,4 +1,4 @@
--- Pass an unsorted array as input & sort (ascending, in-place) using the algorithm of your choice.
+-- Pass an unsorted array as input & sort (ascending) using the algorithm of your choice.
 -- Works for both List<String> and List<Number> (but NOT List<String & Number>)
 local Sort = {}
 
@@ -6,6 +6,7 @@ local Sort = {}
 -- Worst space: O(log(n))
 -- Low and high are optional parameters (nil recommended)
 -- Stable: No
+-- In-Place: Yes
 function Sort.QuickSort(arr, low, high)
 	local function partition(arr, low, high)
 		local pivot = arr[high]
@@ -63,25 +64,98 @@ end
 
 -- Avg time: O(nlog(n))
 -- Worst space: O(n)
-function Sort.MergeSort(arr) end
+-- Stable: Yes
+-- In-Place: No
+function Sort.MergeSort(arr)
+	local function merge(a, l, m, r)
+	    local n1 = m - l + 1
+		local n2 = r - m
+		local L = {}
+		local R = {}
+
+		for i= 1, n1 do
+			L[i] = a[l + i]
+			
+		end
+
+		for i= 1, n2 do
+			R[i] = a[m + i + 1]
+
+		end
+		
+		local i, j, k = 0, 0, l
+		while i < n1 and j < n2 do
+			if L[i] <= R[j] then
+				a[k] = L[i]
+				i += 1
+
+			else 
+				a[k] = R[j]
+				j += 1
+
+			end
+
+			k += 1
+
+		end
+	
+		while i < n1 do
+			a[k] = L[i]
+			i += 1
+			k += 1
+
+		end
+	
+		while j < n2 do
+			a[k] = R[j]
+			j += 1
+			k += 1
+
+		end
+
+	end
+
+    local width = 1
+    local n = #arr
+
+    while width < n do
+        local l = 0
+
+        while l < n do
+            local r = math.min(l + (width * 2 - 1), n - 1)
+            local m = math.min(l + width - 1, n - 1)
+
+            merge(arr, l, m, r)
+            l += width * 2
+
+		end
+
+        width *= 2
+
+	end
+
+    return arr
+
+end
 
 -- Avg time: O(nlog(n))
 -- Worst space: O(n)
 -- Stable: Yes
-function Sort.TimSort(array)
-	local function insertionSort(array, left, right)
+-- In-Place: Yes
+function Sort.TimSort(arr)
+	local function insertionSort(arr, left, right)
 		for i = left + 1, right do
-			local value = array[i]
+			local value = arr[i]
 			local j = i - 1
-			while j >= left and array[j] > value do
-				array[j + 1] = array[j]
+			while j >= left and arr[j] > value do
+				arr[j + 1] = arr[j]
 				j = j - 1
 			end
-			array[j + 1] = value
+			arr[j + 1] = value
 		end
 	end
 
-	local function merge(array, left, middle, right)
+	local function merge(arr, left, middle, right)
 		local leftSize = middle - left + 1
 		local rightSize = right - middle
 
@@ -91,10 +165,10 @@ function Sort.TimSort(array)
 
 		-- Copy data to temp arrays
 		for i = 1, leftSize do
-			leftArray[i] = array[left + i - 1]
+			leftArray[i] = arr[left + i - 1]
 		end
 		for i = 1, rightSize do
-			rightArray[i] = array[middle + i]
+			rightArray[i] = arr[middle + i]
 		end
 
 		-- Merge the temp arrays back into array[left..right]
@@ -103,10 +177,10 @@ function Sort.TimSort(array)
 		local k = left
 		while i <= leftSize and j <= rightSize do
 			if leftArray[i] <= rightArray[j] then
-				array[k] = leftArray[i]
+				arr[k] = leftArray[i]
 				i = i + 1
 			else
-				array[k] = rightArray[j]
+				arr[k] = rightArray[j]
 				j = j + 1
 			end
 			k = k + 1
@@ -114,33 +188,33 @@ function Sort.TimSort(array)
 
 		-- Copy the remaining elements of leftArray[], if there are any
 		while i <= leftSize do
-			array[k] = leftArray[i]
+			arr[k] = leftArray[i]
 			i = i + 1
 			k = k + 1
 		end
 
 		-- Copy the remaining elements of rightArray[], if there are any
 		while j <= rightSize do
-			array[k] = rightArray[j]
+			arr[k] = rightArray[j]
 			j = j + 1
 			k = k + 1
 		end
 	end
 
-	local function timSort(array, left, right)
+	local function timSort(arr, left, right)
 		local size = right - left + 1
 		if size <= 16 then
-			insertionSort(array, left, right)
+			insertionSort(arr, left, right)
 			return
 		end
 
 		local middle = math.floor((left + right) / 2)
-		timSort(array, left, middle)
-		timSort(array, middle + 1, right)
-		merge(array, left, middle, right)
+		timSort(arr, left, middle)
+		timSort(arr, middle + 1, right)
+		merge(arr, left, middle, right)
 	end
 
-	timSort(array, 1, #array)
+	timSort(arr, 1, #arr)
 end
 
 return Sort
